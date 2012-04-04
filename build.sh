fe71b9cb398d91a58fd2d85c817c59d8cd60b80f
Scott Buchanan <buchanan.sc@gmail.com>
2012-04-04 03:44:30 -0700

    * Removed the icon, it wasn't mine * Small tweaks to the build script

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 SCRIPT_PATH="$(readlink -f "$0" 2>/dev/null || greadlink -f "$0")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 
-path_output="${SCRIPT_DIR}/build/PicView.air"
-path_appxml="${SCRIPT_DIR}/application.xml"
-path_src="${SCRIPT_DIR}/src/."
-path_cert="${SCRIPT_DIR}/cert.p12"
+path_output="build/PicView.air"
+path_appxml="application.xml"
+path_src="src/."
+path_cert="cert.p12"
 cert_pass="picviewpass"
 
 [ ! -d "$(dirname $path_output)" ] && mkdir -p "$(dirname $path_output)"
